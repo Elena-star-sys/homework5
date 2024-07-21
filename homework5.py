5e0d0d23c7b4144c75622fdc9c40ce0a3453552a
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-21 23:15:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+a = "string"
+b = int('30')
+c = float("12.5")
+d = True
+immutable_var = a,b,c,d
+tuple = a, b, c, d
+print(tuple)
+food = [a, b, c, d]
+print(food)
+food [2] = 55
+print(food)
+mutable_list = ["дом", "работа", "учёба", "отдых"]
+print(mutable_list)
+mutable_list[0] = "лень"
+print(mutable_list)
